94b40f32ccace497871476c26bee53d316191c4f
bbasa1 <benjamin.basa@ensae.fr>
2023-01-07 16:19:38 +0100

    Ouverture

--- 00_Exploration.R
+++ 00_Exploration.R
@@ -1,1 +1,36 @@
-### TEST TEST TEST 
+library(readr)
+# install.packages("data.table")
+library(data.table)
+# install.packages("ggplot2")
+library(ggplot2)
+# install.packages('viridis')
+library(viridis)
+library(dplyr)
+# install.packages("ggpmisc")
+library(ggpmisc)
+# install.packages("tibble")
+library(magrittr) # needs to be run every time you start R and want to use %>%
+# install.packages("broom")
+# install.packages("ggpubr")
+library(ggpmisc)
+library(tibble)
+library(dplyr)
+library(quantreg)
+# install.packages("tidyverse")
+library(tidyverse) 
+library(broom)
+library(ggpubr)
+
+
+path <- "C:/Users/Benjamin/Desktop/IWEPS/HFCS_UDB_3_2_ASCII/H1.csv"
+path
+data_complete <- read_csv(path, 
+                        locale = locale(encoding ="UTF-8"),
+                        show_col_types = FALSE)
+
+data_complete <- as.data.table(data_complete)
+
+
+data_belgique <- data_complete[SA0100 == 'BE',]
+
+nrow(data_belgique)
