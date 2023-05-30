3a281073aa26079ffea6e4626ec2597d9605aa23
bbasa1 <bbasa@phare.normalesup.org>
2023-05-30 17:28:58 +0200

    Diff_versions

--- 00_main.R
+++ 00_main.R
@@ -2,6 +2,12 @@
 ########################### SCRIPT PRINCIPAL ###################################
 ################################################################################
 
+
+################################################################################
+# ============================ 01 PARAMETRES ===================================
+################################################################################
+
+
 # Le dossier général
 repgen <- "C:/Users/Benjamin/Desktop/IWEPS"
 # Les sous-dossiers
@@ -15,7 +21,7 @@ source(paste(repo_prgm , "01_packages.R" , sep = "/"))
 
 
 # Les sous-sous-dossiers qui contiennent les données
-liste_sous_fichiers_data <- c("HFCS_UDB_1_5_ASCII", "HFCS_UDB_2_4_ASCII", "HFCS_UDB_3_2_ASCII", "HFCS_UDB_4_0 _early_diss_ASCII")
+liste_sous_fichiers_data <- c("HFCS_UDB_1_5_ASCII", "HFCS_UDB_2_4_ASCII", "HFCS_UDB_3_2_ASCII", "HFCS_UDB_4_0_early_diss_ASCII")
 sous_repo_data <- paste(repo_data, liste_sous_fichiers_data, sep = "/")
 
 # Importation des données
@@ -27,7 +33,11 @@ sous_repo_data <- paste(repo_data, liste_sous_fichiers_data, sep = "/")
 
 
 num_table <- 1 ### Change les poids assignés par eurostat
-num_vague_max <- 3 ### Le nombre de vague qu'on veut concaténer ATTENTION la dernière vague a des noms de colonnes en MINUSCULE. Ca pose pbm dans la concaténation...
+num_vague_max <- 4 ### Le nombre de vague qu'on veut concaténer ATTENTION la dernière vague a des noms de colonnes en MINUSCULE. Ca pose pbm dans la concaténation...
+
+################################################################################
+# ============================ 02 IMPORTATION ==================================
+################################################################################
 
 
 source(paste(repo_prgm , "02_importation_data.R" , sep = "/"))
@@ -35,6 +45,59 @@ source(paste(repo_prgm , "02_importation_data.R" , sep = "/"))
 
 table(data_complete$SA0200)
 table(data_complete$SA0100)
+table(data_complete$VAGUE)
 colnames(data_complete)
+
+
+########## Pour étudier les différences entre les différentes versions
+
+data_complete_1 <- copy(data_complete)
+
+num_table <- 2 ### Change les poids assignés par eurostat
+source(paste(repo_prgm , "02_importation_data.R" , sep = "/"))
+data_complete_2 <- copy(data_complete)
+
+
+num_table <- 1 ### Change les poids assignés par eurostat
+source(paste(repo_prgm , "02_importation_data.R" , sep = "/"))
+head(data_complete$HW0010,100)
+
+table(data_complete_1[data_complete_1$DN3001 != data_complete_2$DN3001])
+
+data_complete_1[data_complete_1$SA0010  != data_complete_2$SA0010]
+
+table(data_complete_1  == data_complete_2)
+
+head(data_complete_1[data_complete_1$DOEINHERIT  != data_complete_2$DOEINHERIT ]$DOEINHERIT , 10)
+head(data_complete_2[data_complete_1$DOEINHERIT  != data_complete_2$DOEINHERIT ]$DOEINHERIT , 10)
+
+nrow(data_complete_1)*length(colnames(data_complete_1))
+2277054 + 37663527
+
+
+
+
+########## A discuter : 
+# Les changements entre les fichiers ne viennent pas des poids mais de certaines valeurs. J'imagine les valeurs imputées ?
+# En tout cas elles ont l'air de relativement peu changer entre les différents fichiers, de l'ordre de qq % en général
+# Etats des lieux : Il existe au moins une différence entre les deux premières imputations dans 2277054/59424064 = 4% des cases où il y a du changement
+# Pour obtenir ce chiffre = table(data_complete_1  == data_complete_2) effectifs FALSE/nrow(data_complete_1)*length(colnames(data_complete_1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- 02_importation_data.R
+++ 02_importation_data.R
@@ -1,3 +1,16 @@
+################################################################################
+# ============================ 02 IMPORTATION ==================================
+################################################################################
+
+# Structure du script : 
+#   - Initialisation de la boucle sur la première vague
+#   - Boucle jusqu'à arriver à la dernière vague voulue num_vague_max
+#   
+# ATTENTION : On fusionne code housold uniquement sur les variables années et pays, sinon on ne garde que derivated variables
+# ATTENTION : Les noms de colonnes sont globalement passées en majuscule à cause de la dernière vague d'enquête qui met tout en majuscule
+
+
+
 liste_cols_core_household <- c("sa0010", # household identification number
                                "sa0100", # country
                                "sa0200", # survey vintage
@@ -16,7 +29,7 @@ data_derivated <- as.data.table(data_derivated)
 data_derivated
 nrow(data_derivated)
 try(setnames(data_derivated, "id", "ID"), silent=TRUE) #Parfois les colonnes ont des noms en minuscule...
-
+try(setnames(data_derivated, "survey", "Survey"), silent=TRUE) # Cette variable peut avoir deux noms 
 
 ###### Pour avoir le pays et l'année de référence il faut le household, mais on n'a pas besoin du reste
 path_h <- paste(sous_repo_data,"/h",num_table,".csv", sep = "")
@@ -33,17 +46,20 @@ data_house <- data_house[,..liste_cols_core_household_new]
 nrow(data_house)
 
 
-data_complete <- merge(data_derivated, data_house, by= c("ID", "SA0100"))
+data_complete <- merge(data_derivated, data_house, by= c("ID", "SA0100", "SA0010"))
+data_complete[, VAGUE := 1]
 nrow(data_complete)
 
 
+print("=========================================")
+print(paste("Vague numéro", num_vague))
 print(paste("Validation par le nombre de ligne =", (nrow(data_complete) == nrow(data_derivated))&(nrow(data_complete) == nrow(data_house))))
 print(paste("Nombre de ligne tot =", nrow(data_complete)))
 print(paste("Nombre de colonnes tot =", length(colnames(data_complete))))
-print("=================")
+print("=========================================")
 
 
-for(num_vague in c(2,num_vague_max)){
+for(num_vague in 2:num_vague_max){
 
   # Derivated
   path_d <- paste(sous_repo_data,"/d",num_table,".csv", sep = "")
@@ -51,7 +67,25 @@ for(num_vague in c(2,num_vague_max)){
                              locale = locale(encoding ="UTF-8"),
                              show_col_types = FALSE)
   data_derivated <- as.data.table(data_derivated)
-  try(setnames(data_derivated, "id", "ID"), silent=TRUE) #Parfois les colonnes ont des noms en minuscule...
+  
+  #### Cas particulier : La quatrième vague
+  if(num_vague == 4){ ### On doit passer toutes les colonnes en majuscules dans ce cas
+    for(nom_col in colnames(data_derivated)){
+      try(setnames(data_derivated, nom_col, toupper(nom_col)), silent=TRUE)
+    }
+    print("Dernière vague prise en compte ==> Toutes les colonnes sont passées en majuscules")
+    for(nom_col in colnames(data_complete)){
+      try(setnames(data_complete, nom_col, toupper(nom_col)), silent=TRUE)
+    }
+  }else{ #Sinon seule id est passée en majuscule
+    try(setnames(data_derivated, "id", "ID"), silent=TRUE) #Parfois les colonnes ont des noms en minuscule...
+  }
+  
+  
+  try(setnames(data_derivated, "survey", "SURVEY"), silent=TRUE) # Cette variable peut avoir deux noms 
+  try(setnames(data_derivated, "Survey", "SURVEY"), silent=TRUE) # Cette variable peut avoir deux noms 
+  
+  
   
   # Household
   path_h <- paste(sous_repo_data,"/h",num_table,".csv", sep = "")
@@ -66,66 +100,75 @@ for(num_vague in c(2,num_vague_max)){
   data_house <- data_house[,..liste_cols_core_household_new]
   
   # Merge et concat
-  data_complete_loc <- merge(data_derivated, data_house, by= c("ID", "SA0100"))
+  data_complete_loc <- merge(data_derivated, data_house, by= c("ID", "SA0100", "SA0010"))
   
-  colnames(data_complete_loc)
-  colnames(data_complete)
+  # colnames(data_complete_loc)
+  # colnames(data_complete)
+
+  data_complete_loc[, VAGUE := num_vague]
+  
   
   data_complete <- rbindlist(list(data_complete,
                                   data_complete_loc), fill=TRUE)
   
+  print("=========================================")
+  print(paste("Vague numéro", num_vague))
   print(paste("Validation par le nombre de ligne =", (nrow(data_complete_loc) == nrow(data_derivated))&(nrow(data_complete_loc) == nrow(data_house))))
   print(paste("Nombre de ligne tot =", nrow(data_complete)))
   print(paste("Nombre de colonnes tot =", length(colnames(data_complete))))
-  print("=================")
+  print("=========================================")
+  
 }
 
-#### Les dernières retouches : quelques variables ont des .x et .y
-all(data_complete$SA0100.x == data_complete$SA0100.y)
 
-
+# 
+# num_vague <- 4
+# if(num_vague == 4){
+#   print("Dernière vague prise en compte ==> Toutes les colonnes sont passées en majuscules")
+#   for(nom_col in colnames(data_complete)){
+#     try(setnames(data_complete, nom_col, toupper(nom_col)), silent=TRUE)
+#   }
+# }
+# 
+# # Derivated
 # path_d <- paste(sous_repo_data,"/d",num_table,".csv", sep = "")
 # data_derivated <- read_csv(path_d[num_vague], 
 #                            locale = locale(encoding ="UTF-8"),
 #                            show_col_types = FALSE)
-# 
 # data_derivated <- as.data.table(data_derivated)
-# data_derivated
-# nrow(data_derivated)
-# try(setnames(data_derivated, "id", "ID"), silent=TRUE) #Parfois les colonnes ont des noms en minuscule...
 # 
+# if(num_vague == 4){ ### On doit passer toutes les colonnes en majuscules dans ce cas
+#   for(nom_col in colnames(data_derivated)){
+#     try(setnames(data_derivated, nom_col, toupper(nom_col)), silent=TRUE)
+#   }
+# }
+# # data_derivated
+# # data_complete
+# # 
+# # colnames(data_derivated)
 # 
-# 
-# 
-# ###### Pour avoir le pays et l'année de référence il faut le household, mais on n'a pas besoin du reste
+# # Household
 # path_h <- paste(sous_repo_data,"/h",num_table,".csv", sep = "")
 # data_house <- read_csv(path_h[num_vague], 
 #                        locale = locale(encoding ="UTF-8"),
 #                        show_col_types = FALSE)
-# 
 # data_house <- as.data.table(data_house)
 # for(nom_col in liste_cols_core_household){
-#   print(nom_col)
 #   try(setnames(data_house, nom_col, toupper(nom_col)), silent=TRUE)
 # }
 # liste_cols_core_household_new <- toupper(liste_cols_core_household)
 # data_house <- data_house[,..liste_cols_core_household_new]
-# nrow(data_house)
+# 
+# # Merge et concat
+# data_complete_loc <- merge(data_derivated, data_house, by= c("ID", "SA0100"))
+# 
+# # colnames(data_complete_loc)
+# # colnames(data_complete)
+# 
+# data_complete_loc[, Vague := num_vague]
 # 
 # 
-# 
-# data_complete_loc <- merge(data_derivated, data_house, by= "ID") 
-# data_complete <- rbindlist(list(data_complete,
-#                               data_complete_loc), fill=TRUE)
-# 
-# 
-# 
+# data_complete_2 <- rbindlist(list(data_complete,
+#                                 data_complete_loc), fill=TRUE)
 # 
 # length(colnames(data_complete_2))
-# length(colnames(data_complete))
-# length(colnames(data_complete_loc))
-# setdiff(colnames(data_complete_loc),colnames(data_complete))
-# 
-# data_complete_loc$DOABLETOSAVE
-# 
-# colnames(data_complete)
