a26466e4555cc0b720095c31ecfadecc6e0621fa
bbasa1 <bbasa@phare.normalesup.org>
2023-05-31 16:28:11 +0200

    Nettoyage_et_concentration_pat

--- 00_main.R
+++ 00_main.R
@@ -44,22 +44,12 @@ source(paste(repo_prgm , "02_importation_data.R" , sep = "/"))
 data_belgique <- data_complete[SA0100 == "BE"]
 
 
+################################################################################
+# ============================  03 NETTOYAGE  ==================================
+################################################################################
 
-table(data_complete$SA0200)
-table(data_complete$SA0100)
-table(data_complete$VAGUE)
-colnames(data_complete)
-
-length(colnames(data_complete))
-
-table(data_complete[SA0100 == "BE"]$DATOP10)
-head(data_complete[SA0100 == "BE"]$DATOP10, 20)
-
-hist(data_complete[SA0100 == "BE"]$DATOP10, breaks=13, col="red")
-
-sous_data <- data_complete[SA0100 == "BE", sum(HW0010), by = DITOP10]
-setorder(sous_data, DITOP10)
-sous_data
+### On sélectionne un jeu réduit de données, et on force leur typage
+source(paste(repo_prgm , "03_nettoyage_data.R" , sep = "/"))
 
 
 ############### Variables d'intérêt
@@ -67,7 +57,7 @@ sous_data
 # DA1000 = Total real assets (+ ventilations plus précises)
 # DA2100 = Total financial assets (+ ventilations plus précises)
 # DA3001 = DA1000 + DA2100 = Total assets
-# DATOP10 = Le décile de gross wealth/richesse brut au sein du pays
+# DATOP10 = Le décile de gross wealth/richesse brute au sein du pays
 # DHAGEH1 = Âge de la personne de référence
 # DHEDUH1 = Education de la personne de référence
 # DHGENDERH1 = Genre de la personne de référence
@@ -85,44 +75,206 @@ sous_data
 # DOEINHERIT = S'attend à recevoir un héritage dans le futur
 # DOGIFTINHER = Montant des cadeaux et héritages reçus ==> ATTENTION : La résidence principale peut être exclue du montant, car elle est comptée dans la question sur le mode d'aquisition de celle-ci
 # DOINHERIT = Substantial inheritance/gift received ==> OSEF non ?
+# VAGUE = La vague
+# SA0200 = L'année
 
-sapply(data_belgique, function(x) sum(is.na(x)))
+liste_var_continues <- c("HW0010", "DA1000", "DA2100", "DA3001", "DI2000", "DI2000EQ", "DI2100", "DL1000", "DN3001", "DNFPOS", "DNHW", "DOGIFTINHER")
+liste_var_categorielles <- c("DATOP10", "DHAGEH1", "DHEDUH1", "DHGENDERH1", "DHLIFESATIS", "DITOP10", "DLTOP10", "DNTOP10", "DOEINHERIT", "VAGUE", "SA0200")
+liste_var_interet <- union(liste_var_continues, liste_var_categorielles)
+sous_data_belgique <- data_belgique[,..liste_var_interet]
+sous_data_belgique <- sous_data_belgique %>% mutate_at(liste_var_continues, as.numeric)
+sous_data_belgique <- sous_data_belgique %>% mutate_at(liste_var_categorielles, as.factor)
+
+sous_data_belgique
+
+summary(sous_data_belgique$HW0010)
+
+################################################################################
+# ====================== 04 STAT DES & GRAPHIQUES ==============================
+################################################################################
+liste_var_groupby <- c("DHGENDERH1", "DATOP10")
+var_normalisation <- 'DHGENDERH1' # Par quelle variable est-ce qu'on normalise ? Généralement le sexe
 
 
-data_belgique$HW0010 <- as.numeric(data_belgique$HW0010)
-data_belgique$DA1000 <- as.numeric(data_belgique$DA1000)
-data_belgique$DA2100 <- as.numeric(data_belgique$DA2100)
-data_belgique$DA3001 <- as.numeric(data_belgique$DA3001)
-data_belgique$DI2000 <- as.numeric(data_belgique$DI2000)
-data_belgique$DI2000eq <- as.numeric(data_belgique$DI2000eq)
-data_belgique$DI2100 <- as.numeric(data_belgique$DI2100)
-data_belgique$DL1000 <- as.numeric(data_belgique$DL1000)
-data_belgique$DN3001 <- as.numeric(data_belgique$DN3001)
-data_belgique$DNFPOS <- as.numeric(data_belgique$DNFPOS)
-data_belgique$DNHW <- as.numeric(data_belgique$DNHW)
-data_belgique$DOGIFTINHER <- as.numeric(data_belgique$DOGIFTINHER)
 
-data_belgique$DATOP10 <- as.factor(data_belgique$DATOP10)
-data_belgique$DHAGEH1 <- as.factor(data_belgique$DHAGEH1)
-data_belgique$DHEDUH1 <- as.factor(data_belgique$DHEDUH1)
-data_belgique$DHGENDERH1 <- as.factor(data_belgique$DHGENDERH1)
-data_belgique$DHLIFESATIS <- as.factor(data_belgique$DHLIFESATIS)
-data_belgique$DITOP10 <- as.factor(data_belgique$DITOP10)
-data_belgique$DLTOP10 <- as.factor(data_belgique$DLTOP10)
-data_belgique$DNTOP10 <- as.factor(data_belgique$DNTOP10)
-data_belgique$DOEINHERIT <- as.factor(data_belgique$DOEINHERIT)
+dots <- lapply(var_normalisation, as.symbol) #Penser à bien convertir pour ne pas avoir de problèmes...
+
+
+data_for_plot <- sous_data_belgique[, sum(HW0010), by = liste_var_groupby] #On calcule les effectifs
+data_for_plot <- data_for_plot %>% group_by(.dots = dots) %>% mutate(new = 100*V1/sum(V1)) # Pour la normalisation il faut faire attention à grouper par sexe
+data_for_plot <- as.data.table(data_for_plot)
+data_for_plot
+
+data_for_plot[, Sexe:= factor(
+  fcase(
+    DHGENDERH1 == 1, "Homme",
+    DHGENDERH1 == 2, "Femme"
+  )
+)
+]
+
+
+titre <- "Patrimoine normalisé par sexe"
+titre_save <- "Patrimoine_sexe"
+titre_save <- paste(repo_sorties, titre_save, sep ='/')
+x <-"DATOP10"
+sortby_x <- "DATOP10"
+y <- "new"
+fill <- "Sexe"
+xlabel <-"Quantile de patrimoine brut"
+ylabel <-"% de la population belge"
+
+ggplot(data = data_for_plot, aes(x = reorder(.data[[x]], .data[[sortby_x]]), y = .data[[y]], fill = .data[[fill]])) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  labs(title=titre,
+       x= xlabel,
+       y= ylabel) + 
+  scale_y_continuous(limits = c(0, 15), labels = function(y) format(y, scientific = FALSE)) + 
+  scale_fill_discrete() +
+  scale_color_viridis() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+
+
+
+######## On regarde la concentration des différents types de patrimoines #############
+
+nb_quantiles <- 100
+
+# On calcule tous les quantiles et les patrimoines moyens par quantiles
+sous_data_belgique[,Quantiles := cut(DA3001,
+                                            breaks=quantile(DA3001, probs=seq(0, 1, by=1/nb_quantiles), na.rm=T),
+                                            include.lowest= TRUE, labels=1:nb_quantiles)]
+data_for_plot <- sous_data_belgique[, mean(DA3001), by = Quantiles]
+setnames(data_for_plot, "V1", "Total")
+
+sous_data_belgique[,Quantiles := cut(DA1000,
+                                            breaks=quantile(DA1000, probs=seq(0, 1, by=1/nb_quantiles), na.rm=T),
+                                            include.lowest= TRUE, labels=1:nb_quantiles)]
+data_for_plot_1 <- sous_data_belgique[, mean(DA1000), by = Quantiles]
+setnames(data_for_plot_1, "V1", "Physique")
+
+sous_data_belgique[,Quantiles := cut(DA2100,
+                                     breaks=quantile(DA2100, probs=seq(0, 1, by=1/nb_quantiles), na.rm=T),
+                                     include.lowest= TRUE, labels=1:nb_quantiles)]
+data_for_plot_2 <- sous_data_belgique[, mean(DA2100), by = Quantiles]
+setnames(data_for_plot_2, "V1", "Financier")
+
+# On merge tout ça pour avoir 1 colonne/type de patrimoine
+data_for_plot <- merge(data_for_plot, data_for_plot_1, by = "Quantiles")
+data_for_plot <- merge(data_for_plot, data_for_plot_2, by = "Quantiles")
+
+# On fait les cumsum
+data_for_plot[, Total := 100*cumsum(Total)/sum(Total)]
+data_for_plot[, Physique := 100*cumsum(Physique)/sum(Physique)]
+data_for_plot[, Financier := 100*cumsum(Financier)/sum(Financier)]
+
+
+data_for_plot
+
+
+melted <- melt(data_for_plot, 
+               id.vars = "Quantiles", 
+               measure.vars  = c("Total", "Physique", "Financier"),
+               variable.name = "variable",
+               value.name    = "value")
+
+melted$Quantiles <- as.numeric(melted$Quantiles)
+
+ggplot(melted) +
+  geom_line(aes(x = Quantiles, y= value, color = variable)) +
+  scale_color_viridis_d() +
+  labs(
+    x = "Part des ménages", 
+    y = "Patrimoine détenu (cumulatif)",
+    color = "Type de patrimoine",
+    titre = "Fonction de répartition du patrimoine des ménages Belges, par type de patrimoine"
+  ) +
+  scale_y_continuous(
+    labels = scales::dollar_format(
+      prefix = "",
+      suffix = " %",
+      big.mark = " ",
+      decimal.mark = ","), 
+    expand = c(0, 0)
+  ) + 
+  scale_x_continuous(
+    labels = scales::dollar_format(
+      prefix = "",
+      suffix = " %",
+      big.mark = " ",
+      decimal.mark = ","),
+    expand = c(0, 0)
+  ) +
+  theme(legend.text = element_text(angle = 0, vjust = 0.7, hjust = 0),
+        axis.text.x = element_text(angle = 45, vjust = 0.5),
+        legend.position = "right")
+
+
+
+
+
+
+### On essaie de passer ça en fonction
+liste_type_patrimoines <- c("DA3001" = "Total",
+                            "DA1000" = "Physique",
+                            "DA2100" = "Financier")
+
+data_for_plot <- as.data.table(1:nb_quantiles)
+setnames(data_for_plot, 'V1', "Quantiles")
+data_for_plot$Quantiles <- as.numeric(data_for_plot$Quantiles)
+data_for_plot
+
+for(type_pat in names(liste_type_patrimoines)){
+  # print(type_pat)
+  sous_data_belgique[,Quantiles := cut(sous_data_belgique[[type_pat]],
+                                       breaks=quantile(sous_data_belgique[[type_pat]], probs=seq(0, 1, by=1/nb_quantiles), na.rm=T),
+                                       include.lowest= TRUE, labels=1:nb_quantiles)]
+  
+  data_for_plot_loc <- sous_data_belgique[, mean(get(type_pat)), by = Quantiles]
+  setnames(data_for_plot_loc, "V1", liste_type_patrimoines[[type_pat]])
+  data_for_plot_loc$Quantiles <- as.numeric(data_for_plot_loc$Quantiles)
+  data_for_plot <- merge(data_for_plot, data_for_plot_loc, by = "Quantiles")
+}
+
+for(type_pat in liste_type_patrimoines){
+  data_for_plot[, eval(type_pat) := 100*cumsum(get(type_pat))/sum(get(type_pat))]
+  
+}
+
+data_for_plot
 
 
 
 ################################################## Un peu d'héritage...
 
-hist(log(data_belgique$DOGIFTINHER), breaks=50, col="red")
+hist(log(sous_data_belgique$DOGIFTINHER), breaks=50, col="red")
 
+sous_data_belgique
+
+sous_data_belgique[, cor(DA3001, DI2000), by = VAGUE]
+
+
+dw <- svydesign(ids = ~1, data = data_belgique, weights = ~ data_belgique$HW0010)
+
+correlation_vague <- 1:num_vague_max
+for(num_vague in 1:num_vague_max){
+  data_loc <- sous_data_belgique[VAGUE == num_vague,]
+  dw_loc <- svydesign(ids = ~1, data = data_loc, weights = ~ data_loc$HW0010)
+  correlation_vague[num_vague] <- svycor(~DA3001 + DI2000, design = dw_loc)$cors[1,2]
+}
+
+correlation_vague
+sous_data_belgique[, cor(DA3001, DI2000), by = VAGUE]
+
+
+
+class(svycor(~DA3001 + DI2000, design = dw))
+
+svycor(~DA3001 + DI2000, design = dw)$cors[1,2]
 
 
 
 ################################################## Exploration avec le paclage survey
-dw <- svydesign(ids = ~1, data = data_belgique, weights = ~ data_belgique$HW0010)
 
 dw$variables$DN3001 # Pour retrouver le contenu d'une colonne
 
